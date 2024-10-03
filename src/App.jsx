6cd0a16cb1a31cef2c0e836337234f83b3bfc6b6
aneesha-byt <aneesha.t.a@gmail.com>
2024-10-03 10:41:22 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Film-Stream" element={<Home />} />
-          g<Route path="/favorites" element={<Favorites />} />
+          <Route path="/favorites" element={<Favorites />} />
           <Route path="/movie/:id" element={<Movieinfo />} />
         </Routes>
       </Layout>
